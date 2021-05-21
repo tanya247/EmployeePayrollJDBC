e0f5625d0a4230ff261893817e4dd5f8ec262a9f
Tanya <82596524+tanya247@users.noreply.github.com>
2021-05-21 07:30:49 +0530

    UC1 Showed

--- src/main/java/DbDemo.java
+++ src/main/java/DbDemo.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.Enumeration;
 
 public class DbDemo {
--    public static void main(String[] args){
+    public static void main(String[] args){
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSS1=false";
         String userName = "root";
         String password = "Ruchi-789";
